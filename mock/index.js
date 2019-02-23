55909499bef82a1388715e3d586c90c7397911b5
ShiQianfeng720 <sqflwh@163.com>
2019-02-23 15:25:10 +0800

    解决测试表格页面的问题

--- mock/index.js
+++ mock/index.js
@@ -1,6 +1,7 @@
 import Mock from 'mockjs'
 import userAPI from './user'
 import tableAPI from './table'
+import articleAPI from './article'
 
 // Fix an issue with setting withCredentials = true, cross-domain request lost cookies
 // https://github.com/nuysoft/Mock/issues/300
@@ -19,6 +20,13 @@ Mock.XHR.prototype.send = function() {
 Mock.mock(/\/user\/login/, 'post', userAPI.login)
 Mock.mock(/\/user\/info/, 'get', userAPI.getInfo)
 Mock.mock(/\/user\/logout/, 'post', userAPI.logout)
+
+// 文章相关
+Mock.mock(/\/article\/list/, 'get', articleAPI.getList)
+Mock.mock(/\/article\/detail/, 'get', articleAPI.getArticle)
+Mock.mock(/\/article\/pv/, 'get', articleAPI.getPv)
+Mock.mock(/\/article\/create/, 'post', articleAPI.createArticle)
+Mock.mock(/\/article\/update/, 'post', articleAPI.updateArticle)
 
 // Table
 Mock.mock(/\/table\/list/, 'get', tableAPI.list)
